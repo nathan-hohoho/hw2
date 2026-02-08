4f6dac19cfc4260872d9a3d5a8993ec701ef0406
nathan-hohoho <nathan.ho@kellogg.northwestern.edu>
2026-02-08 04:34:46 +0000

    updated to save!

--- kmdb.rb
+++ kmdb.rb
@@ -86,7 +86,7 @@ Agent.destroy_all
 # Generate models and tables, according to the domain model.
 warner_bros = Studio.new
 warner_bros["name"] = "Warner Bros."
-warner_bros.save
+warner_bros.save!
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
@@ -95,7 +95,7 @@ batman_begins["title"] = "Batman Begins"
 batman_begins["year_released"] = 2005
 batman_begins["rated"] = "PG-13"
 batman_begins["studio_id"] = warner_bros.id
-batman_begins.save
+batman_begins.save!
 p batman_begins
 
 dark_knight = Movie.new
@@ -103,80 +103,80 @@ dark_knight["title"] = "The Dark Knight"
 dark_knight["year_released"] = 2008
 dark_knight["rated"] = "PG-13"
 dark_knight["studio_id"] = warner_bros.id
-dark_knight.save
+dark_knight.save!
 
 dark_knight_rises = Movie.new
 dark_knight_rises["title"] = "The Dark Knight Rises"
 dark_knight_rises["year_released"] = 2012
 dark_knight_rises["rated"] = "PG-13"
 dark_knight_rises["studio_id"] = warner_bros.id
-dark_knight_rises.save
+dark_knight_rises.save!
 
 # Generate actors
 christian_bale = Actor.new
 christian_bale["name"] = "Christian Bale"
-christian_bale.save
+christian_bale.save!
 
 michael_caine = Actor.new
 michael_caine["name"] = "Michael Caine"
-michael_caine.save
+michael_caine.save!
 
 liam_neeson = Actor.new
 liam_neeson["name"] = "Liam Neeson"
-liam_neeson.save
+liam_neeson.save!
 
 katie_holmes = Actor.new
 katie_holmes["name"] = "Katie Holmes"
-katie_holmes.save
+katie_holmes.save!
 
 gary_oldman = Actor.new
 gary_oldman["name"] = "Gary Oldman"
-gary_oldman.save
+gary_oldman.save!
 
 # Batman Begins
 bruce_wayne = Role.new
 bruce_wayne["movie_id"] = batman_begins.id
 bruce_wayne["actor_id"] = christian_bale.id
 bruce_wayne["character_name"] = "Bruce Wayne"
-bruce_wayne.save
+bruce_wayne.save!
 p bruce_wayne
 
 alfred = Role.new
 alfred["movie_id"] = batman_begins.id
 alfred["actor_id"] = michael_caine.id
 alfred["character_name"] = "Alfred"
-alfred.save
+alfred.save!
 
 ras_al_ghul = Role.new
 ras_al_ghul["movie_id"] = batman_begins.id
 ras_al_ghul["actor_id"] = liam_neeson.id
 ras_al_ghul["character_name"] = "Ra's Al Ghul"
-ras_al_ghul.save
+ras_al_ghul.save!
 
 rachel_dawes = Role.new
 rachel_dawes["movie_id"] = batman_begins.id
 rachel_dawes["actor_id"] = katie_holmes.id
 rachel_dawes["character_name"] = "Rachel Dawes"
-rachel_dawes.save
+rachel_dawes.save!
 
 commissioner_gordon = Role.new
 commissioner_gordon["movie_id"] = batman_begins.id
 commissioner_gordon["actor_id"] = gary_oldman.id
 commissioner_gordon["character_name"] = "Commissioner Gordon"
-commissioner_gordon.save
+commissioner_gordon.save!
 
 # The Dark Knight
 heath_ledger = Actor.new
 heath_ledger["name"] = "Heath Ledger"
-heath_ledger.save
+heath_ledger.save!
 
 aaron_eckhart = Actor.new
 aaron_eckhart["name"] = "Aaron Eckhart"
-aaron_eckhart.save
+aaron_eckhart.save!
 
 maggie_gyllenhaal = Actor.new
 maggie_gyllenhaal["name"] = "Maggie Gyllenhaal"
-maggie_gyllenhaal.save
+maggie_gyllenhaal.save!
 
 # The Dark Knight roles
 tdk_bruce_wayne = Role.new
